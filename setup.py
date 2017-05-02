bc90505605f74eb1e07a0061197c6cb4097c74ee
Lars Kellogg-Stedman <lars@redhat.com>
2017-05-01 21:27:40 -0400

    systemd: replace generator with unit conditionals
    In order to avoid problems caused by changes to upstream unit files,
    this patch completely separates our systemd units from those
    distributed by upstream.  The RHEL unit files can be found in the
    rhel/systemd directory.
    
    This commit replaces the generator with Conditional* statements in the
    unit files. You are still able to disable cloud-init by setting
    cloud-init=disabled on the kernel command line or by touching
    /etc/cloud/cloud-init.disable.
    
    We also retarget the cloud-init services from cloud-init.target back
    to multi-user.target, which resolves the root cause of rhbz#1440831.
    
    Resolves: rhbz#1440831
    X-downstream-only: true

--- setup.py
+++ setup.py
@@ -63,9 +63,6 @@ INITSYS_FILES = {
     'sysvinit_freebsd': [f for f in glob('sysvinit/freebsd/*') if is_f(f)],
     'sysvinit_deb': [f for f in glob('sysvinit/debian/*') if is_f(f)],
     'sysvinit_openrc': [f for f in glob('sysvinit/gentoo/*') if is_f(f)],
-    'systemd': [f for f in (glob('systemd/*.service') +
-                            glob('systemd/*.target')) if is_f(f)],
-    'systemd.generators': [f for f in glob('systemd/*-generator') if is_f(f)],
     'upstart': [f for f in glob('upstart/*') if is_f(f)],
 }
 INITSYS_ROOTS = {
@@ -73,9 +70,6 @@ INITSYS_ROOTS = {
     'sysvinit_freebsd': '/usr/local/etc/rc.d',
     'sysvinit_deb': '/etc/init.d',
     'sysvinit_openrc': '/etc/init.d',
-    'systemd': pkg_config_read('systemd', 'systemdsystemunitdir'),
-    'systemd.generators': pkg_config_read('systemd',
-                                          'systemdsystemgeneratordir'),
     'upstart': '/etc/init/',
 }
 INITSYS_TYPES = sorted([f.partition(".")[0] for f in INITSYS_ROOTS.keys()])
@@ -117,50 +111,8 @@ def read_requires():
     return str(deps).splitlines()
 
 
-# TODO: Is there a better way to do this??
-class InitsysInstallData(install):
-    init_system = None
-    user_options = install.user_options + [
-        # This will magically show up in member variable 'init_sys'
-        ('init-system=', None,
-         ('init system(s) to configure (%s) [default: None]' %
-          (", ".join(INITSYS_TYPES)))),
-    ]
-
-    def initialize_options(self):
-        install.initialize_options(self)
-        self.init_system = ""
-
-    def finalize_options(self):
-        install.finalize_options(self)
-
-        if self.init_system and isinstance(self.init_system, str):
-            self.init_system = self.init_system.split(",")
-
-        if len(self.init_system) == 0:
-            raise DistutilsArgError(
-                ("You must specify one of (%s) when"
-                 " specifying init system(s)!") % (", ".join(INITSYS_TYPES)))
-
-        bad = [f for f in self.init_system if f not in INITSYS_TYPES]
-        if len(bad) != 0:
-            raise DistutilsArgError(
-                "Invalid --init-system: %s" % (','.join(bad)))
-
-        for system in self.init_system:
-            # add data files for anything that starts with '<system>.'
-            datakeys = [k for k in INITSYS_ROOTS
-                        if k.partition(".")[0] == system]
-            for k in datakeys:
-                self.distribution.data_files.append(
-                    (INITSYS_ROOTS[k], INITSYS_FILES[k]))
-        # Force that command to reinitalize (with new file list)
-        self.distribution.reinitialize_command('install_data', True)
-
-
 if in_virtualenv():
     data_files = []
-    cmdclass = {}
 else:
     data_files = [
         (ETC + '/cloud', glob('config/*.cfg')),
@@ -176,11 +128,6 @@ else:
             [f for f in glob('doc/examples/seed/*') if is_f(f)]),
         ('/usr/lib/udev/rules.d', [f for f in glob('udev/*.rules')]),
     ]
-    # Use a subclass for install that handles
-    # adding on the right init system configuration files
-    cmdclass = {
-        'install': InitsysInstallData,
-    }
 
 
 requirements = read_requires()
@@ -198,7 +145,6 @@ setuptools.setup(
     scripts=['tools/cloud-init-per'],
     license='Dual-licensed under GPLv3 or Apache 2.0',
     data_files=data_files,
-    cmdclass=cmdclass,
     entry_points={
         'console_scripts': [
             'cloud-init = cloudinit.cmd.main:main'
